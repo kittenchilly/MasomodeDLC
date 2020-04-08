b5adad2ffe1c01fcc95f9d2c5e7afb6cad8117ae
Thomas <tailsguy24@gmail.com>
2020-04-08 14:50:13 -0400

    doll
    doll

---
+++ Calamity/Items/AntimatterDoll.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace MasomodeDLC.Calamity.Items
+{
+	public class AntimatterDoll : ModItem
+	{
+		public override bool Autoload(ref string name)
+		{
+			return ModLoader.GetMod("CalamityMod") != null;
+		}
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Antimatter Doll");
+			Tooltip.SetDefault("Summons...the void...???\n"
+							 + "Alters the appearance, greed, and food preferences of the void if it is already present");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 28;
+			item.height = 36;
+			item.value = Item.sellPrice(0, 0, 0, 0);
+			item.rare = 11;
+			ModLoader.GetMod("CalamityMod").Call("SetItemRarity", this, 17);
+			item.defense = 0;
+			item.accessory = true;
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual)
+		{
+			player.GetModPlayer<MasoDLCPlayer>().antimatterDoll = true;
+		}
+
+		public override bool AllowPrefix(int pre)
+		{
+			return false;
+		}
+	}
+}

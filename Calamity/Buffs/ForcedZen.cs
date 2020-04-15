2a80b907f39c731ffc96a359be06057aacd688f5
Poshymilk <starlitsky43@gmail.com>
2020-04-14 22:49:00 -0700

    fix Autoload return code

--- Calamity/Buffs/ForcedZen.cs
+++ Calamity/Buffs/ForcedZen.cs
@@ -19,7 +19,7 @@ namespace MasomodeDLC.Calamity.Buffs
         public override bool Autoload(ref string name, ref string texture)
         {
             texture = "MasomodeDLC/Calamity/PlaceholderCalamityDebuff.png";
-            return true;
+            return ModLoader.GetMod("CalamityMod") != null;
         }
 
         public override void Update(Player player, ref int buffIndex)
